da6a87bc86961d033f0d95f5228344fa64a93ae3
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-05 17:25:26 +0200

    price repetition fix

--- js/main.js
+++ js/main.js
@@ -14,15 +14,13 @@ btnSend.addEventListener("click",
 
         document.getElementById('details-name').innerHTML = fullName;
 
+        let price = km * 0.21;
+
         if (age == 'over65') {
-            let price = km * 0.21;
             document.getElementById('details-price').innerHTML = Math.round(((price / 100) * 60) * 100) / 100 + "euro";
         } else if (age == 'minorenne') {
-            let price = km * 0.21;
             document.getElementById('details-price').innerHTML = Math.round(((price / 100) * 80) * 100) / 100 + "euro";
-
         } else {
-            let price = km * 0.21;
             document.getElementById('details-price').innerHTML = Math.round((price) * 100) / 100 + "euro";
         }
     }
